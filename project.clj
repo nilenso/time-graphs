26b33b39cb71044b1c610806b56c160f624e492a
Shafeeq Kunnakkadan and Srihari Sriraman <pair+shafeeq+srihari@nilenso.com>
2016-12-12 16:40:07 +0530

    First cut.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0"]
+                 [incanter "1.5.7"]
+                 [cheshire "5.6.3"]
+                 [clj-time "0.12.2"]])
