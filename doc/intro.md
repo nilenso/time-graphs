fc5e9bff4d2517512997ea53b297a0eb030b996b
Shafeeq Kunnakkadan and Srihari Sriraman <pair+shafeeq+srihari@nilenso.com>
2016-12-12 14:55:13 +0530

    Add lein skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to harvest-times
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/harvest_times/core_test.clj
@@ -0,0 +1,7 @@
+(ns harvest-times.core-test
+  (:require [clojure.test :refer :all]
+            [harvest-times.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
